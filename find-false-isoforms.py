94f784a78f2089122df9e37cbc8fc2b99335844d
Camille Scott <camille.scott.w@gmail.com>
2015-09-11 17:07:57 -0700

    Add isoform val script

--- find-false-isoforms.py
+++ find-false-isoforms.py
@@ -22,20 +22,31 @@ def search_sequence(node, seq, threshold):
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sbt-file')
-    parser.add_argument('--search', nargs='+')
+    parser.add_argument('--sbt')
+    parser.add_argument('--assembly', nargs='+')
     parser.add_argument('--threshold', type=float, default=0.9)
     parser.add_argument('--print-tree', action='store_true', default=False)
     args = parser.parse_args()
-
+    
+    # Load the Sequence Bloom Tree
     tree = sbt.load_sbt(args.sbt_file)
 
-    if args.search:
-        for fn in args.search:
+    for fn in args.assembly:
+        with open(fn + '.good', 'wb') as good_fp, open(fn + '.bad', 'wb') as bad_fp:
             print '*** Searching', fn
+            n_good = 0
+            n_bad = 0
             for record in screed.open(fn):
-                print '---\n', record.name
-                print [x.metadata for x in tree.find(search_sequence, record.sequence, args.threshold)]
+                results = tree.find(search_sequence, record.sequence, args.threshold)
+                if results:
+                    n_good += 1
+                    good_fp.write('>{name}:[{support}]\n{seq}'.format(
+                        name=record.name, support=','.join([x.name for x in results]),
+                        seq=record.sequence))
+                else:
+                    n_bad += 1
+                    bad_fp.write('>{name}\n{seq}'.format(name=record.name, seq=record.sequence))
+            print n_good, 'good', n_bad, 'bad transcripts'
 
     if args.print_tree:
         sbt.print_sbt(tree)
